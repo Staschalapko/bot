3a995a4d04468f7ba557d9dfff786167b2f02b74
stanislavchalapko <Staschalapko@gmail.com>
2024-07-31 15:52:01 +0300

    test

--- playwright.config.js
+++ playwright.config.js
@@ -22,11 +22,10 @@ const config = {
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries:  0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : 3,
+  workers: process.env.CI ? 4 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -34,12 +33,22 @@ const config = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 15 * 1000,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://rem-servis.site/lend/ru/',
-    headless: true,
+    baseURL: 'https://rem-servis.site/?utm_source=google&utm_medium=cpc&utm_campaign=cid_20976708202&utm_target=&utm_group=gid153377157970&utm_content=%D0%A0%D0%B5%D0%BC%D0%BE%D0%BD%D1%82%20%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA%D0%BE%D0%B2&utm_team=harkiv1&utm_term%D1%80%D0%B5%D0%BC%D0%BE%D0%BD%D1%82%20%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA%D0%BE%D0%B2%20%D1%85%D0%B0%D1%80%D1%8C%D0%BA%D0%BE%D0%B2&utm_tab=help-pc&gad_source=1&gclid=Cj0KCQjwwae1BhC_ARIsAK4JfrwnvE1qTnBOmSiaXGRlD2p9KdtWqbjfk8UDF1TOvHUQjNxTRxPbT_EaApGBEALw_wcB',
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    video: 'on-first-retry',
+    trace: 'on',
+    video: 'on',
   },
+  reporter:  process.env.CI
+      ? [
+        ['html'],
+        ['playwright-qase-reporter',
+          {
+            logging: true,
+          },
+        ],
+      ]
+      : [['html']],
   projects: [
     {
       name: 'chrome',
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,50 +1,35 @@
 const { test } = require('@playwright/test');
-const { LoginPage } = require('../pages/login-page');
-const { DashboardPage } = require('../pages/dashboard/dashboard-page');
-const { updateTestResults } = require('./../helpers/saveTestResults.js');
-const { qase } = require('playwright-qase-reporter/dist/playwright');
+const { BasePage } = require('../pages/base-page');
 
-test(qase(35,'ON-8 Login with an email address'), async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.acceptCookie();
-  await loginPage.enterEmail(process.env.LOGIN_EMAIL);
-  await loginPage.enterPwd(process.env.LOGIN_PWD);
-  await loginPage.clickLoginButton();
-  const dashboardPage = new DashboardPage(page);
-  await dashboardPage.isHeaderDisplayed('Projects');
+
+test('Call', async ({ page }) => {
+  const basePage = new BasePage(page);
+  await basePage.goto();
+  await basePage.clickCall();
+  await basePage.enterData();
+  await basePage.clickSubmit();
 });
 
-test(qase(40,'ON-13 Login with invalid email address'), async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.acceptCookie();
-  await loginPage.enterEmail('test@com');
-  await loginPage.enterPwd(process.env.LOGIN_PWD);
-  await loginPage.isEmailInputErrorDisplayed('Enter a valid email please');
-  await loginPage.isLoginButtonDisplayed();
-  await loginPage.isLoginButtonDisabled();
+test('Call2', async ({ page }) => {
+  const basePage = new BasePage(page);
+  await basePage.goto();
+  await basePage.clickCall();
+  await basePage.enterData();
+  await basePage.clickSubmit();
 });
 
-test(qase(41,'ON-14 Login with no password'), async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.enterEmail(process.env.LOGIN_EMAIL);
-  await loginPage.clickPwdInput();
-  await loginPage.clickHeader();
-  await loginPage.isLoginButtonDisplayed();
-  await loginPage.isLoginButtonDisabled();
+test('Call3', async ({ page }) => {
+  const basePage = new BasePage(page);
+  await basePage.goto();
+  await basePage.clickCall();
+  await basePage.enterData();
+  await basePage.clickSubmit();
 });
 
-test(qase(42,'ON-15 Login with incorrect password'), async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.enterEmail(process.env.LOGIN_EMAIL);
-  await loginPage.enterPwd('11223344');
-  await loginPage.clickLoginButton();
-  await loginPage.isLoginErrorMessageDisplayed('Email or password is incorrect.');
-});
-
-test.afterEach(async ({ page }, testInfo) => {
-  await updateTestResults(testInfo.status, testInfo.retry)
+test('Call4', async ({ page }) => {
+  const basePage = new BasePage(page);
+  await basePage.goto();
+  await basePage.clickCall();
+  await basePage.enterData();
+  await basePage.clickSubmit();
 });
