d6de2427fbbf467320979560fd60c9a54b0d6bb6
stanislavchalapko <Staschalapko@gmail.com>
2024-07-31 16:56:37 +0300

    test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,35 +1,40 @@
 const { test } = require('@playwright/test');
 const { BasePage } = require('../pages/base-page');
 
+const runTestMultipleTimes = async (testFunction, times) => {
+  for (let i = 0; i < times; i++) {
+    await testFunction();
+  }
+};
 
-test('Call', async ({ page }) => {
+const runCallTest = async (page) => {
   const basePage = new BasePage(page);
   await basePage.goto();
   await basePage.clickCall();
   await basePage.enterData();
   await basePage.clickSubmit();
+};
+
+test('Call', async ({ page }) => {
+  await runTestMultipleTimes(async () => {
+    await runCallTest(page);
+  }, 1000);
 });
 
 test('Call2', async ({ page }) => {
-  const basePage = new BasePage(page);
-  await basePage.goto();
-  await basePage.clickCall();
-  await basePage.enterData();
-  await basePage.clickSubmit();
+  await runTestMultipleTimes(async () => {
+    await runCallTest(page);
+  }, 1000);
 });
 
 test('Call3', async ({ page }) => {
-  const basePage = new BasePage(page);
-  await basePage.goto();
-  await basePage.clickCall();
-  await basePage.enterData();
-  await basePage.clickSubmit();
+  await runTestMultipleTimes(async () => {
+    await runCallTest(page);
+  }, 1000);
 });
 
 test('Call4', async ({ page }) => {
-  const basePage = new BasePage(page);
-  await basePage.goto();
-  await basePage.clickCall();
-  await basePage.enterData();
-  await basePage.clickSubmit();
+  await runTestMultipleTimes(async () => {
+    await runCallTest(page);
+  }, 1000);
 });
